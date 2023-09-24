929544d3de79fbcbf753476c675e54ae23f97fbe
MelihKrts <melih.karatas1281@gmail.com>
2023-09-24 11:11:42 +0300

    save changes

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,10 +1,10 @@
 export default function Slide() {
   return (
-    <section className="w-full mt-16 relative">
+    <section className="w-full  relative">
       <div className="w-full bg-[url('../assets/images/slide-one.jpg')] bg-cover bg-bottom bg-fixed h-[500px]">
         <div className="container mx-auto">
           <div className="w-full flex flex-col justify-center items-center mx-auto relative">
-            <div className="translate-y-1/2 absolute top-1/2">
+            <div className="translate-y-1/2 absolute top-1/2 pt-16">
           <h1 className="text-4xl lg:text-5xl md:text-3xl font-bold text-center text-white">
               Happy homes, happy people and lives
             </h1>
@@ -12,7 +12,7 @@ export default function Slide() {
               Proin non rhoncus dolor. Nunc bibendum tempus dolor. Praesent
               vulputate.
             </p>
-            <button className="px-8 py-2 bg-button text-white text-center rounded-md">Contact Us</button>
+            <button className="px-8 py-2 bg-button text-white text-center rounded-md left-[40%] relative">Contact Us</button>
 
             </div>
           </div>
