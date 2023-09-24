e817712808bcef891e0737b8111b9bbf437ff80c
MelihKrts <melih.karatas1281@gmail.com>
2023-09-24 11:01:13 +0300

    save changes

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,20 +1,20 @@
 export default function Slide() {
   return (
-    <section className="w-full bg-[url('../assets/images/slide-one.jpg')] h-[65vh] bg-cover bg-bottom bg-fixed">
-      <div className="container mx-auto ">
-        <div className="w-full flex justify-center items-center">
-
-          <div className="absolute lg:top-1/2 left-1/2 text-center w-full  -translate-x-1/2 -translate-y-1/2">
-            <h1 className="text-4xl lg:text-5xl font-bold text-center text-white">
+    <section className="w-full mt-16 relative">
+      <div className="w-full bg-[url('../assets/images/slide-one.jpg')] bg-cover bg-bottom bg-fixed h-[500px]">
+        <div className="container mx-auto">
+          <div className="w-full flex flex-col justify-center items-center mx-auto relative">
+            <div className="translate-y-1/2 absolute top-1/2">
+          <h1 className="text-4xl lg:text-5xl md:text-3xl font-bold text-center text-white">
               Happy homes, happy people and lives
             </h1>
-            <p className="my-4 opacity-80 text-white">
+            <p className="my-4 opacity-80 text-white md:text-sm lg:text-base text-center">
               Proin non rhoncus dolor. Nunc bibendum tempus dolor. Praesent
               vulputate.
             </p>
-
             <button className="px-8 py-2 bg-button text-white text-center rounded-md">Contact Us</button>
 
+            </div>
           </div>
         </div>
       </div>
